4069b863a4bc1fa68e55dc29f15e47ef53513651
huanngo012 <huanngo1612@gmail.com>
2023-11-04 20:00:37 +0700

    feat: add specialty router

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const { notFound, errHandler } = require("../middlewares/errHandler");
 const userRouter = require("./userRouter");
 const doctorRouter = require("./doctorRouter");
 const clinicRouter = require("./clinicRouter");
+const specialtyRouter = require("./specialtyRouter");
 
 const initRoutes = (app) => {
   app.use("/api/user", userRouter);
   app.use("/api/doctor", doctorRouter);
   app.use("/api/clinic", clinicRouter);
+  app.use("/api/specialty", specialtyRouter);
 
   app.use(notFound);
   app.use(errHandler);
