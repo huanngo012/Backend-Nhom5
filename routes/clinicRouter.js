6725235ca776dffe1b383b283146cd382b7bde89
huanngo012 <huanngo1612@gmail.com>
2023-11-04 20:00:08 +0700

    feat: add clinic router

---
+++ routes/clinicRouter.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const ctrls = require("../controllers/clinicController");
+const {
+  verifyAccessToken,
+  isHost,
+  isAdmin,
+} = require("../middlewares/verifyToken");
+
+router.get("/", ctrls.getAllClinics);
+router.get("/count", ctrls.getCountClinic);
+router.get("/:id", ctrls.getClinic);
+router.post("/", [verifyAccessToken, isAdmin], ctrls.addClinic);
+router.put("/:id", [verifyAccessToken, isAdmin], ctrls.updateClinic);
+router.delete("/:id", [verifyAccessToken, isAdmin], ctrls.deleteClinic);
+router.put("/host/:id", [verifyAccessToken, isHost], ctrls.updateClinicByHost);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const { notFound, errHandler } = require("../middlewares/errHandler");
 const userRouter = require("./userRouter");
 const doctorRouter = require("./doctorRouter");
+const clinicRouter = require("./clinicRouter");
 
 const initRoutes = (app) => {
   app.use("/api/user", userRouter);
   app.use("/api/doctor", doctorRouter);
+  app.use("/api/clinic", clinicRouter);
 
   app.use(notFound);
   app.use(errHandler);
