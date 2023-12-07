94ae4db4086d13681986a0621637ce2ea0cccc01
huanngo012 <huanngo1612@gmail.com>
2023-12-07 19:53:42 +0700

    feat: limit storage img

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.use(
 );
 
 //image
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "300mb" }));
+app.use(express.urlencoded({ extended: true, limit: "300mb" }));
 
 //database
 dbConnect();
