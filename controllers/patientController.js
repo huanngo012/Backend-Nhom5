524e8c31d9e21e65a81d5b857302a15797cc110d
huanngo012 <huanngo1612@gmail.com>
2024-07-19 09:36:00 +0700

    test: test date

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -70,14 +70,13 @@ const addPatient = asyncHandler(async (req, res) => {
   const { fullName, phone, gender, dob } = req.body;
   if (!fullName || !phone || !gender || !dob)
     throw new Error("Vui lòng nhập đầy đủ");
-  req.body.dob = new Date(+dob);
+  req.body.dob = new Date(+req.body.dob);
   req.body.dob.setHours(0, 0, 0, 0);
   req.body.dob.setDate(req.body.dob.getDate());
   const response = await Patient.create({ ...req.body, bookedBy: _id });
   return res.status(200).json({
     success: response ? true : false,
     data: response ? response : "Thêm hồ sơ bệnh nhân thất bại",
-    dob: dob,
   });
 });
 const updatePatient = asyncHandler(async (req, res) => {
@@ -87,22 +86,19 @@ const updatePatient = asyncHandler(async (req, res) => {
     throw new Error("Vui lòng nhập đầy đủ");
   const patient = await Patient.find({ bookedBy: _id });
   if (!patient) throw new Error("Bạn không có quyền chỉnh sửa");
-  const before = req.body.dob;
 
   if (req.body.dob) {
     req.body.dob = new Date(+req.body.dob);
     req.body.dob.setHours(0, 0, 0, 0);
     req.body.dob.setDate(req.body.dob.getDate());
   }
-  const after = req.body.dob;
+
   const response = await Patient.findByIdAndUpdate(id, req.body, {
     new: true,
   });
   return res.status(200).json({
     success: response ? true : false,
     data: response ? response : "Cập nhật hồ sơ bệnh nhân thất bại",
-    before,
-    after,
   });
 });
 
